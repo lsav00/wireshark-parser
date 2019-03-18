1c0a64ef4554d1099359b6568bf5b1d0db973ed8
Larry Savidge <savidge00@gmail.com>
2019-03-18 03:36:24 -0400

    Create pcapper3.py

---
+++ pcapper3.py
@@ -0,0 +1,37 @@
+"""pcapper3 grabs protocols"""
+
+import os
+import subprocess
+import time
+
+tshark_loc = os.path.join("C:\\", "Program Files", "Wireshark")
+os.chdir(tshark_loc)
+
+with open("C:\\users\\savid\\Desktop\\malware_analysis\\output3.txt") as stdout:
+	subprocess.Popen("tshark -G column-formats", shell=True, stdout=stdout, stderr=stdout)
+
+#tshark -r C:\Users\savid\Desktop\malware_analysis\homies2.pcap -T fields -e frame.protocols | findstr "data"	
+
+"""Don't forget DOUBLE SLASHES"""
+tsharkcmd1 = "tshark" 
+tsharkcmd1 += " -r C:\\Users\\savid\\Desktop\\malware_analysis\\homies2.pcap -T fields -e frame.protocols"
+
+
+with open("C:\\users\\savid\\Desktop\\malware_analysis\\output4.txt", "w") as stdout:
+	subprocess.Popen(tsharkcmd1, shell=True, stdout=stdout, stderr=stdout)
+
+time.sleep(1)
+	
+mylist=[]
+	
+with open("C:\\users\\savid\\Desktop\\malware_analysis\\output4.txt", 'r') as input_file:
+	for line in input_file:
+		if line not in mylist:
+			mylist.append(line)
+
+#with open("C:\\users\\savid\\Desktop\\malware_analysis\\output5.txt", 'w') as output:
+for x in mylist:
+	x=x.strip(" ")
+	x=x.strip("\n")
+	print(x)
+		
